ae862646e92a736a38b64dbee57a638f8701ce18
zwyu123 <zwyu2017@163.com>
2020-02-26 18:02:08 +0800

    Spu详情数据查询

---
+++ src/main/java/com/lin/missyou/model/Spu.java
@@ -0,0 +1,31 @@
+package com.lin.missyou.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
+@Getter
+@Setter
+public class Spu extends BaseEntity {
+    @Id
+    private Long id;
+    private String title;
+    private String subtitle;
+    private Long categoryId;
+    private Long rootCategoryId;
+    private Boolean online;
+    private String price;
+    private Long sketchSpecId;
+    private Long defaultSkuId;
+    private String img;
+    private String discountPrice;
+    private String description;
+    private String tags;
+    private Boolean isTest;
+
+    private String forThemeImg;
+}
