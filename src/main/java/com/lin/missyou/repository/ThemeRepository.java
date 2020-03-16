2da89d8e85d16a513162c3f4171094b32f1df374
zwyu123 <zwyu2017@163.com>
2020-03-16 22:58:11 +0800

    Theme接口

---
+++ src/main/java/com/lin/missyou/repository/ThemeRepository.java
@@ -0,0 +1,16 @@
+package com.lin.missyou.repository;
+
+import com.lin.missyou.model.Theme;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ThemeRepository extends JpaRepository<Theme, Long> {
+
+    @Query("select t from Theme t where t.name in (:names)")
+    List<Theme> findByNames(List<String> names);
+
+    Optional<Theme> findByName(String name);
+}
